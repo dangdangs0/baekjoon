b0e44d6aa6cc54ffce444a85e44bd79b43a3358e
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-05 17:10:10 +0900

    Create 18108.java

---
+++ 입출력과 사칙연산/18108.java
@@ -0,0 +1,16 @@
+//2023.04.05
+
+import java.util.Scanner;
+
+public class Code18108 {
+	public static void main(String[] args) {
+		int year;
+		
+		Scanner sc=new Scanner(System.in);
+		year=Integer.valueOf(sc.next());
+		
+		System.out.println(year-543);
+		
+	}
+}
+
