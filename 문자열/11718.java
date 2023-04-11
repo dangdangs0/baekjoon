9418601c675189d7ef7cad9d4c4a357895091c47
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-11 15:31:04 +0900

    Create 11718.java

---
+++ 문자열/11718.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Code11718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		String line;
+		
+		while(scanner.hasNext()) {
+			line=scanner.nextLine();
+			System.out.println(line);
+		}
+		scanner.close();
+	}
+
+}
