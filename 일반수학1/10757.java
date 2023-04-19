f5bc6999a07c6408bfe25a0579b07211aef03233
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-19 13:54:22 +0900

    Create 10757.java

---
+++ 일반수학1/10757.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+public class Code10757 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+		BigInteger a=scanner.nextBigInteger();
+		BigInteger b=scanner.nextBigInteger();
+		
+		System.out.println(a.add(b));
+	}
+
+}
