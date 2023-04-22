7b42bbce81c8fd92d6815d03bced79e6d30a1c01
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-22 12:17:13 +0900

    Create 27323.java

---
+++ 기하 : 직사각형과 삼각형/27323.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Code27323 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+		int A,B;
+		A=scanner.nextInt();
+		B=scanner.nextInt();
+		
+		System.out.println(A*B);
+	}
+
+}
