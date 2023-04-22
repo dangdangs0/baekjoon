eeee380d2b57ce1ad423edba8c109a78873d8868
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-22 13:23:50 +0900

    Create 15894.java

---
+++ 기하 : 직사각형과 삼각형/15894.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Code15894 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+		long n=scanner.nextInt();
+		
+		System.out.println(n*4);
+	}
+
+}
