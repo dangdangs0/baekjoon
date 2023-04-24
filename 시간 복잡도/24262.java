ad3e3f65c288ad5c939655d461377ef111ee54c9
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-24 12:56:50 +0900

    Create 24262.java

---
+++ 시간 복잡도/24262.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code24262 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+		int n=scanner.nextInt();
+		
+		System.out.println(1);
+		System.out.println(0);
+	}
+
+}
