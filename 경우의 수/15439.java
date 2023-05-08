4e0e2045ff8303c9cd4b2c8ffa3811c6d68b8b84
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-08 22:14:55 +0900

    Create 15439.java

---
+++ 경우의 수/15439.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Code15439 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+
+        System.out.println((N-1)*N);
+    }
+}
