b6ea4017fe90c7ca6d23f1d8205122e36c4c1524
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-08 22:22:03 +0900

    Create 24723.java

---
+++ 경우의 수/24723.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Code24723 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+
+        System.out.println((int)Math.pow(2,N));
+    }
+}
