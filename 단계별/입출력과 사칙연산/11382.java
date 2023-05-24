f60a9083e7d1ea4ad3dd705ac3607ae068ad9613
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-24 16:45:41 +0900

    Add files via upload

---
+++ 단계별/입출력과 사칙연산/11382.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Code11382 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		long a,b,c;
+		long total;
+
+		a=scanner.nextLong();
+		b=scanner.nextLong();
+		c=scanner.nextLong();
+
+		total=a+b+c;
+		System.out.println(total);
+		
+	}
+}
