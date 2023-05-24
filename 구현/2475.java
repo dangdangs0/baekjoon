ad0552cd4c2b62a758a0944fa0729c523c811dce
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-24 16:09:07 +0900

    Create 2475.java

---
+++ 구현/2475.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code2475 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int answer=0;
+
+        for(int i=0;i<5;i++){
+            answer+=(int)Math.pow(sc.nextInt(),2);
+        }
+
+        System.out.println(answer%10);
+
+    }
+}
