44aabd121408eb1520f35ad75a02ec44e4a76426
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-26 17:20:02 +0900

    Create 4101.java

---
+++ 난이도별/BronzeV/4101.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Code4101 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+
+            if(a==0 && b==0){
+                break;
+            }
+            else{
+                if(a>b){
+                    System.out.println("Yes");
+                }
+                else{
+                    System.out.println("No");
+                }
+            }
+        }
+    }
+}
