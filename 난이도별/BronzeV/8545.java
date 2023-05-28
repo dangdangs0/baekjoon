178b98ea8f14a5fb1517dda6fd53b7a7d99d58de
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-28 17:31:46 +0900

    Create 8545.java

---
+++ 난이도별/BronzeV/8545.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Code8545 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder(sc.next());
+
+        System.out.println(sb.reverse());
+    }
+}
