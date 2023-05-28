2ab4ddcf3a63816b120b27986bb4ee1f742798c6
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-28 17:38:42 +0900

    Create 8871.java

---
+++ 난이도별/BronzeV/8871.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code8871 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        n=n+1;//시범 라운드
+        //2개~3개
+        System.out.println(n*2+" "+n*3);
+    }
+}
