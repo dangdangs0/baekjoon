ebb6847a8ed89077a219122b036f5c308ca57c00
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-29 16:10:26 +0900

    Create 10189.java

---
+++ 난이도별/BronzeV/10189.java
@@ -0,0 +1,8 @@
+public class Code10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
