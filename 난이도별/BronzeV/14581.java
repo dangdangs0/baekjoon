f3aa4176eb20d570d6eb54466a90186d950b2ba0
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-30 15:30:49 +0900

    Create 14581.java

---
+++ 난이도별/BronzeV/14581.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code14581 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String hongjun=sc.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+hongjun+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
