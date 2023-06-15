a6f4cd3d25ac2aabeb91e8bb992d447971e4fc22
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-06-15 13:21:21 +0900

    Create 16394.java

---
+++ 난이도별/BronzeV/16394.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code16394 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int y=sc.nextInt();
+
+        int answer=y-1946;
+        System.out.println(answer);
+    }
+}
