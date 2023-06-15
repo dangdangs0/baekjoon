dcb3cff8e2f336b5aaa18cf273306d8ad95a1163
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-06-15 13:19:28 +0900

    Create 16170.java

---
+++ 난이도별/BronzeV/16170.java
@@ -0,0 +1,7 @@
+public class Code16170 {
+    public static void main(String[] args) {
+        System.out.println(2023);
+        System.out.println("06");
+        System.out.println(15);
+    }
+}
