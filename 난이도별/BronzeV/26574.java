39b137a94a0986ae756edd1c86552edaf7d62a47
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-06-24 17:47:13 +0900

    Create 26574.java

---
+++ 난이도별/BronzeV/26574.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Code26574 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int count=sc.nextInt();
+
+        for(int i=0;i<count;i++){
+            int num=sc.nextInt();
+
+            System.out.println(num+" "+num);
+        }
+    }
+}
