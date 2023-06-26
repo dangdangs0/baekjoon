4e0935963c9e49e39c88ad005282cc2b79999885
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-06-26 13:19:51 +0900

    Create 26766.java

---
+++ 난이도별/BronzeV/26766.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Code26766 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     ");
+        }
+
+    }
+}
