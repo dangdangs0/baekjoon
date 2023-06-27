4435d0239bcdd0bd6be7e3f90d8d5983d93c0c87
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-06-27 12:12:04 +0900

    Create 27389.java

---
+++ 난이도별/BronzeV/27389.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code27389 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        double answer=n/4.0;
+        System.out.println(answer);
+    }
+}
