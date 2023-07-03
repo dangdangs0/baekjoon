2f4565bd740a89f23b8a4e61e82fa16a67187742
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-07-03 12:06:37 +0900

    Create 5524.java

---
+++ 난이도별/BronzeIV/5524.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Code5524 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            String si=sc.next();
+
+            System.out.println(si.toLowerCase());
+        }
+    }
+}
