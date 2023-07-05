486b4290cba344c897d7ab66984f4e3c59fd4b4a
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-07-05 13:04:12 +0900

    Create 6749.java

---
+++ 난이도별/BronzeIV/6749.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code6749 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int last=sc.nextInt();
+        int mid=sc.nextInt();
+        int inter=mid-last;
+        System.out.println(mid+inter);
+    }
+}
