ef551962d06b231bbaeac16267b8e983182dab1b
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-07-10 11:29:01 +0900

    Create 6916.java

---
+++ 난이도별/BronzeIV/6916.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+public class Code6916 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        switch (num){
+            case 0:
+                System.out.println(" * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *");
+                break;
+            case 1:
+                System.out.println("\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *");
+                break;
+            case 2:
+                System.out.println(" * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *\n" +
+                        "*\n" +
+                        "*\n" +
+                        "*\n" +
+                        " * * *");
+                break;
+            case 3:
+                System.out.println(" * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *");
+                break;
+            case 4:
+                System.out.println("\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *");
+                break;
+            case 5:
+                System.out.println(" * * *\n" +
+                        "*\n" +
+                        "*\n" +
+                        "*\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *");
+                break;
+            case 6:
+                System.out.println(" * * *\n" +
+                        "*\n" +
+                        "*\n" +
+                        "*\n" +
+                        " * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *");
+                break;
+            case 7:
+                System.out.println(" * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *");
+                break;
+            case 8:
+                System.out.println(" * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *");
+                break;
+            case 9:
+                System.out.println(" * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *");
+                break;
+        }
+    }
+}
